255b34214f6ad2f6725b1f79426110ed716d51c8
jyothirmayee <jyothirmayee.24.6.1997@gmail.com>
2018-08-08 16:10:47 +0530

    initial

---
+++ src/app/task1/component1/component1.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-component1',
+  templateUrl: './component1.component.html',
+  styleUrls: ['./component1.component.css']
+  // template:`<button (click)="onClick()">Print</button>{{text}}` 
+})
+export class Component1Component  {
+private text;
+  // constructor() { }
+  printText(text){
+    console.log(text);
+  }
+
+
+}
---
+++ src/app/task2/task2.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component2Component } from './component2/component2.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [Component2Component]
+})
+export class Task2Module { }
